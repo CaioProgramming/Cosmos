97177d58ed35d4a182bcb1b6fad380bab0d4a935
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-06-01 12:57:32 -0300

    fixed imports

--- composeApp/src/commonMain/kotlin/features/home/ui/components/GalleryCard.kt
+++ composeApp/src/commonMain/kotlin/features/home/ui/components/GalleryCard.kt
@@ -12,42 +12,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
-import theme.fadingEdge
-import theme.shapeRadius10
-import theme.shapeRadius5
+import utils.fadingEdge
 
 @Composable
-fun GalleryCard(title: String,
-                description: String,
-                thumbnail: String) {
-
+fun GalleryCard(
+    title: String,
+    description: String,
+    thumbnail: String,
+) {
     Box(
-        modifier = Modifier
-            .fillMaxWidth(0.5f)
-            .height(400.dp) ) {
+        modifier =
+            Modifier
+                .fillMaxWidth(0.5f)
+                .height(400.dp),
+    ) {
         KamelImage(
             resource = asyncPainterResource(data = thumbnail),
             contentDescription = title,
             modifier = Modifier.fillMaxSize().fadingEdge(),
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
         )
         Column(modifier = Modifier.align(Alignment.BottomStart)) {
             Text(
                 text = title,
                 modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
-                style = MaterialTheme.typography.body1
+                style = MaterialTheme.typography.body1,
             )
 
             Text(
                 text = description,
                 modifier = Modifier.padding(horizontal = 8.dp, vertical = 2.dp),
-                style = MaterialTheme.typography.caption
+                style = MaterialTheme.typography.caption,
             )
         }
-
     }
 }
